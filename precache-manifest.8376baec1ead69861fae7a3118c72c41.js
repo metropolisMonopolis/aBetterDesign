cb10bc20ff89e4c9f87227c17ef7bf5ce3283653
Kevin Montes de Oca <kevinmon97@gmail.com>
2019-12-12 17:13:08 -0600

    Updates

---
+++ precache-manifest.8376baec1ead69861fae7a3118c72c41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e58c1eae00e89ec67a74a1b75d2f0ac2",
+    "url": "/aBetterDesign/index.html"
+  },
+  {
+    "revision": "f3582d4e80b6ade6379c",
+    "url": "/aBetterDesign/static/css/main.3e48dd11.chunk.css"
+  },
+  {
+    "revision": "f5e2aef39e22e9cb621f",
+    "url": "/aBetterDesign/static/js/2.e3409c1e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/aBetterDesign/static/js/2.e3409c1e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f3582d4e80b6ade6379c",
+    "url": "/aBetterDesign/static/js/main.8536f74f.chunk.js"
+  },
+  {
+    "revision": "2954ea285ea3b3e9b346",
+    "url": "/aBetterDesign/static/js/runtime-main.bdea3006.js"
+  }
+]);
